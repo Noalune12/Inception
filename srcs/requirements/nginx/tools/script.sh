4625c490c4c62e5734786a3bf633b7934f6f4ac8
lbuisson <lbuisson@Inception.myguest.virtualbox.org>
2025-09-02 14:31:30 +0200

    nginx: order files

--- srcs/requirements/nginx/tools/script.sh
+++ srcs/requirements/nginx/tools/script.sh
@@ -1,3 +1,3 @@
 #!/bin/sh 
 openssl req -x509 -newkey rsa:4096 -keyout /etc/nginx/ssl/key.pem -out /etc/nginx/ssl/cert.pem -sha256 -days 365 -nodes -subj "/C=XX/ST=StateName/L=CityName/O=CompanyName/OU=CompanySectionName/CN=CommonNameOrHostname"
-nginx -g "daemon off;"
+exec nginx -g "daemon off;"

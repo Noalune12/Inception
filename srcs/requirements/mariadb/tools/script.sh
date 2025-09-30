fea75816c94dce97e87273fc805baa66f9054cb2
lbuisson <lbuisson@Inception.myguest.virtualbox.org>
2025-09-30 13:54:59 +0200

    close port mariadb + get env var

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -28,18 +28,26 @@ if [ ! -d "${MYSQL_DATADIR}/mysql" ]; then
     mariadb -u ${MYSQL_ROOT_USER} -p"${MYSQL_ROOT_PWD}" -e "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};"
     # mariadb -u ${MYSQL_ROOT_USER} -p"${MYSQL_ROOT_PWD}" -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'localhost' IDENTIFIED BY '$MYSQL_USER_PWD';"
     mariadb -u ${MYSQL_ROOT_USER} -p"${MYSQL_ROOT_PWD}" -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_USER_PWD';"
-    
     mariadb -u ${MYSQL_ROOT_USER} -p"${MYSQL_ROOT_PWD}" -e "ALTER USER '${MYSQL_ROOT_USER}'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PWD}';"
-    
-    
+
     mariadb -u ${MYSQL_ROOT_USER} -p"${MYSQL_ROOT_PWD}" -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';"
     # mariadb -u ${MYSQL_ROOT_USER} -p"${MYSQL_ROOT_PWD}" -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'localhost';"
 
     mariadb -u ${MYSQL_ROOT_USER} -p"${MYSQL_ROOT_PWD}" -e "GRANT ALL PRIVILEGES on *.* to 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PWD';"
+    # mariadb -u ${MYSQL_ROOT_USER} -p"${MYSQL_ROOT_PWD}" -e "GRANT SHUTDOWN ON *.* TO '${MYSQL_USER}'@'%';"
+    # mariadb -u ${MYSQL_ROOT_USER} -p"${MYSQL_ROOT_PWD}" -e "GRANT RELOAD ON *.* TO '${MYSQL_USER}'@'%';"
 
+
+    # mariadb -u ${MYSQL_ROOT_USER} -p"${MYSQL_ROOT_PWD}" -e "DROP USER IF EXISTS '${MYSQL_ROOT_USER}'@'localhost';"
     mariadb -u ${MYSQL_ROOT_USER} -p"${MYSQL_ROOT_PWD}" -e "FLUSH PRIVILEGES;"
 
+    # mariadb -u ${MYSQL_ROOT_USER} -p"${MYSQL_ROOT_PWD}" -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');"
+    # mariadb -u ${MYSQL_ROOT_USER} -p"${MYSQL_ROOT_PWD}" -e "UPDATE mysql.user SET plugin='unix_socket', authentication_string='' WHERE User='root' AND Host='localhost';"
+
+    # mariadb -u ${MYSQL_USER} -p"${MYSQL_USER_PWD}" -e "FLUSH PRIVILEGES;"
+
     mariadb-admin -u ${MYSQL_ROOT_USER} -p"${MYSQL_ROOT_PWD}" shutdown
+    # mariadb-admin -u ${MYSQL_USER} -p"${MYSQL_USER_PWD}" shutdown
 fi
 
 
--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -8,22 +8,22 @@ if [ ! -f wp-config.php ]; then
     wp core download --allow-root || true
     wp config create --allow-root \
         --dbhost=mariadb:3306 \
-        --dbuser=lbuisson \
-        --dbpass=UsTGHe1y0eUD4yBO5Mqe \
-        --dbname=wordpress
+        --dbuser=${MYSQL_USER} \
+        --dbpass=${MYSQL_USER_PWD} \
+        --dbname=${MYSQL_DATABASE}
     wp core install --allow-root \
         --skip-email \
-        --url="http://lbuisson.42.fr" \
+        --url="http://$DOMAIN" \
         --title=Inception \
-        --admin_user=lbuisson42 \
-        --admin_password=pb9i65XB3dd3Zx4lzoLM \
-        --admin_email=lbuisson@gmail.com
-    wp option update siteurl http://lbuisson.42.fr --allow-root
-    wp option update home http://lbuisson.42.fr --allow-root
+        --admin_user=${WP_ADMIN} \
+        --admin_password=${WP_ADMIN_PWD} \
+        --admin_email=${WP_ADMIN_MAIL}
+    wp option update siteurl http://$DOMAIN --allow-root
+    wp option update home http://$DOMAIN --allow-root
 
     # Create a regular user if it doesn't already exist
-    if ! wp user get user42 --allow-root > /dev/null 2>&1; then
-        wp user create user42 user@gmail.com --role=author --user_pass=1234 --allow-root
+    if ! wp user get $WP_USER --allow-root > /dev/null 2>&1; then
+        wp user create $WP_USER $WP_USER_MAIL --role=author --user_pass=$WP_USER_PWD --allow-root
     fi
 else
     echo "WordPress is already installed."

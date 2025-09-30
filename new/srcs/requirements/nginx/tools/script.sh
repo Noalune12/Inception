73a514a20a0a66b1613662588238f3cdc0ab03d7
Lou-Anne Buisson <lbuisson@z1r2p4.42lyon.fr>
2025-09-30 11:02:29 +0200

    rm unused directory

--- new/srcs/requirements/nginx/tools/script.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh 
-openssl req -x509 -newkey rsa:4096 -keyout ${SSL_KEY} -out ${SSL_CERT} -sha256 -days 365 -nodes -subj "/CN=lbuisson.42.fr" \
-    >/dev/null 2>/dev/null
-exec nginx -g "daemon off;"

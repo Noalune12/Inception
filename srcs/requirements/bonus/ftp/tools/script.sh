7c3a8f9dbcdd0b0dbcce0d06d4c08a5695443c4e
lbuisson <lbuisson@Inception.myguest.virtualbox.org>
2025-10-06 19:08:26 +0200

    ftp: add passive to check on terminal

--- srcs/requirements/bonus/ftp/tools/script.sh
+++ srcs/requirements/bonus/ftp/tools/script.sh
@@ -4,15 +4,21 @@ set -e
 
 FTP_PWD=$(cat /run/secrets/ftp_pwd)
 
+
+HOST_IP=$(ip route | awk '/default/ { print $3 }')
+echo "pasv_address=${HOST_IP}" >> /etc/vsftpd/vsftpd.conf
+
 echo "Waiting for Wordpress volume to be ready..."
 while [ ! -f /var/www/html/.wp-built ]; do
     sleep 2
 done
 echo "Wordpress is ready, starting FTP"
 
-adduser -D -G www-data -h /var/www/html $FTP_USER
+if ! id "$FTP_USER" &>/dev/null; then
+    adduser -D -G www-data -h /var/www/html $FTP_USER
+fi
 echo "$FTP_USER:$FTP_PWD" | chpasswd 
-chown -R ftpuser:www-data /var/www/html
+# chown -R ftpuser:www-data /var/www/html
 chmod -R 755 /var/www/html
 rm /var/www/html/.wp-built
 
